bfb9b0528427f13b52a55c0d75c51ab86728d873
saisharan062 <24cse062.saisharanpatro@giet.edu>
2026-08-18 10:50:41 +0530

    Add files via upload

---
+++ Assignment-2/Assignment-2/A-2(q-6,7,8)output.py
@@ -0,0 +1,25 @@
+Python 3.14.2 (tags/v3.14.2:df79316, Dec  5 2025, 17:18:21) [MSC v.1944 64 bit (AMD64)] on win32
+Enter "help" below or click "Help" above for more information.
+>>> 
+= RESTART: C:/Users/Asus/OneDrive/Desktop/bubu/DMDW LAB ASSIGNMENT/Assignment-2/A-2,Q-6.py
+Enter a series of numbers: 10 12 15 18 20 20 22 25
+Mean: 17.75
+Median: 19.0
+Mode: 20
+Variance: 25.928571428571427
+Standard Deviation: 5.092010548749033
+>>> 
+= RESTART: C:/Users/Asus/OneDrive/Desktop/bubu/DMDW LAB ASSIGNMENT/Assignment-2/A-2,Q-7.py
+Mean: 32.22222222222222
+Median: 30.0
+Mode: 10
+Variance: 228.39506172839506
+Standard Deviation: 15.112745009706048
+>>> 
+= RESTART: C:/Users/Asus/OneDrive/Desktop/bubu/DMDW LAB ASSIGNMENT/Assignment-2/A-2,Q-8.py
+0.2172156384862992
+Random choice from list: 100
+Random choice from another list: 6
+Random number using randrange: 28
+Another random number: 0.033825182357816175
+After seed: 0.6229016948897019
